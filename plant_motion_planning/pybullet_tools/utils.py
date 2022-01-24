fc6ea0d33e22c5e9db9b36333242d907c4d571c2
Rajin <rnagpal@RNAGPAL-UBUNTU-20.04>
2022-01-24 11:31:00 -0500

    Load in Val and Husky model

--- plant_motion_planning/pybullet_tools/utils.py
+++ plant_motion_planning/pybullet_tools/utils.py
@@ -73,6 +73,8 @@ ROOMBA_URDF = 'models/turtlebot/roomba.urdf'
 TURTLEBOT_URDF = 'models/turtlebot/turtlebot_holonomic.urdf'
 DRAKE_IIWA_URDF = 'models/drake/iiwa_description/urdf/iiwa14_polytope_collision.urdf'
 DRAKE_IIWA_URDF_EDIT = 'models/drake/iiwa_description/urdf/iiwa14_polytope_collision_edit.urdf'
+HDT_MICHIGAN_URDF = "models/husky_custom_description/urdf/mic09_description.urdf"
+
 WSG_50_URDF = 'models/drake/wsg_50_description/urdf/wsg_50_mesh_visual.urdf' # wsg_50 | wsg_50_mesh_visual | wsg_50_mesh_collision
 #SCHUNK_URDF = 'models/drake/wsg_50_description/sdf/schunk_wsg_50.sdf'
 PANDA_HAND_URDF = "models/franka_description/robots/hand.urdf"
@@ -85,7 +87,7 @@ KUKA_IIWA_GRIPPER_SDF = 'kuka_iiwa/kuka_with_gripper.sdf'
 R2D2_URDF = 'r2d2.urdf'
 MINITAUR_URDF = 'quadruped/minitaur.urdf'
 HUMANOID_MJCF = 'mjcf/humanoid.xml'
-HUSKY_URDF = 'husky/husky.urdf'
+#HUSKY_URDF = 'husky/husky.urdf'
 RACECAR_URDF = 'racecar/racecar.urdf' # racecar_differential.urdf
 PR2_GRIPPER = 'pr2_gripper.urdf'
 PANDA_URDF = 'franka_panda/panda.urdf'
--- scripts/our_method_single_branch.py
+++ scripts/our_method_single_branch.py
@@ -18,7 +18,7 @@ from plant_motion_planning import cfg
 from plant_motion_planning.pybullet_tools.kuka_primitives import BodyConf, Command, get_free_motion_gen_with_angle_constraints_v4
 from plant_motion_planning.pybullet_tools.utils import enable_gravity, connect, dump_world, set_pose, \
     draw_global_system, set_camera_pose, Pose, Point, Euler, BLOCK_URDF, load_model, disconnect, update_state, \
-    disable_real_time, HideOutput, DRAKE_IIWA_URDF_EDIT, save_state
+    disable_real_time, HideOutput, HDT_MICHIGAN_URDF, save_state, step_simulation
 import pybullet as p
 from plant_motion_planning.utils import envs, generate_tall_plants
 import argparse
@@ -97,6 +97,8 @@ def main():
     connect(use_gui=True,width=1920, height=1080)
     disable_real_time()
 
+    enable_gravity()
+
     # Set camera pose to desired position and orientation
     set_camera_pose((0.0, -1.06, 1.5),(0,0,0))
 
@@ -106,20 +108,23 @@ def main():
     draw_global_system()
     with HideOutput():
         # Load robot model given macro to urdf file and fix its base
-        robot = load_model(DRAKE_IIWA_URDF_EDIT, fixed_base = True) # KUKA_IIWA_URDF | DRAKE_IIWA_URDF
+        robot = load_model(HDT_MICHIGAN_URDF, fixed_base = False) # KUKA_IIWA_URDF | DRAKE_IIWA_URDF
         # Load floor model from urdf file and fix its base
         floor = load_model(os.path.join(cfg.ROOT_DIR, 'models/short_floor.urdf'), fixed_base=True)
+
+    for t in range(0, 50):
+        step_simulation()
 
     # Load a block that we would like to use as our object of interest
     block = load_model(BLOCK_URDF, fixed_base=True)
     # Set location and orientation of block in the simulation world
-    set_pose(block, Pose(Point(x=0.4,y=-0.4,z=0.45),Euler(yaw=1.57)))
+    #set_pose(block, Pose(Point(x=0.4,y=-0.4,z=0.45),Euler(yaw=1.57)))
 
     # Get plant positions given the kind of placement of plants required
     plant_positions = envs[args.env]
 
     # Generate plants given positions
-    plant_ids, plant_representations = generate_tall_plants(num_plants=len(plant_positions), positions=plant_positions, floor=floor)
+    #plant_ids, plant_representations = generate_tall_plants(num_plants=len(plant_positions), positions=plant_positions, floor=floor)
 
     dump_world()
 
@@ -128,37 +133,38 @@ def main():
 
     # Save the state of the world before beginning the planning process
     saved_world = p.saveState()
-
-    # Create a BodyConf object with the initial configuration stored in it
-    conf_i = BodyConf(robot, configuration=init_conf)
-    # Create a BodyConf object with the final or end configuration stored in it
-    conf_g = BodyConf(robot, configuration=goal_conf)
-
-    # Moving arm from conf_i to conf_g and avoiding the block, floor. The plants are deflected within their limits to
-    # reach the goal
-    command = move_arm_conf2conf(robot, [floor, block], plant_representations, deflection_limit,
-                                 conf_i, conf_g)
-
-    if (command is None):
-        print('Unable to find a plan!')
-        print("*********************************************************")
-        return
-
-    # Restore the state of the simulation after the planning process
-    p.restoreState(saved_world)
-    update_state()
-
-    # Save a video of the execution if required
-    if(args.video_filename != None):
-        log_id = p.startStateLogging(loggingType=p.STATE_LOGGING_VIDEO_MP4, fileName=args.video_filename)
-
-    command.execute_with_controls(robot, init_conf, block, plant_ids, plant_representations,
-                                                            deflection_limit)
-    print('Quitting simulation')
-
-    # shutting down logger
-    if(args.video_filename != None):
-        p.stopStateLogging(log_id)
+    #
+    # # Create a BodyConf object with the initial configuration stored in it
+    # conf_i = BodyConf(robot, configuration=init_conf)
+    # # Create a BodyConf object with the final or end configuration stored in it
+    # conf_g = BodyConf(robot, configuration=goal_conf)
+    #
+    # # Moving arm from conf_i to conf_g and avoiding the block, floor. The plants are deflected within their limits to
+    # # reach the goal
+    # command = move_arm_conf2conf(robot, [floor, block], plant_representations, deflection_limit,
+    #                              conf_i, conf_g)
+    #
+    # if (command is None):
+    #     print('Unable to find a plan!')
+    #     print("*********************************************************")
+    #     return
+    #
+    # # Restore the state of the simulation after the planning process
+    # p.restoreState(saved_world)
+    # update_state()
+    #
+    # # Save a video of the execution if required
+    # if(args.video_filename != None):
+    #     log_id = p.startStateLogging(loggingType=p.STATE_LOGGING_VIDEO_MP4, fileName=args.video_filename)
+    #
+    # command.execute_with_controls(robot, init_conf, block, plant_ids, plant_representations,
+    #                                                         deflection_limit)
+    # print('Quitting simulation')
+    #
+    # # shutting down logger
+    # if(args.video_filename != None):
+    #     p.stopStateLogging(log_id)
+    input("wait for enter")
     disconnect()
 
 if __name__ == '__main__':
