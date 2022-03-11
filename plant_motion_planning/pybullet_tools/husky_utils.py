313d9a5a77c2854d437c7203934a95f1c202b5d3
Rajin Nagpal <rnagpal02@outlook.com>
2022-03-10 22:14:29 -0500

    Using steering function for motion primitives

--- plant_motion_planning/pybullet_tools/husky_utils.py
+++ plant_motion_planning/pybullet_tools/husky_utils.py
@@ -4,6 +4,7 @@ import rospkg
 import torch
 import yaml
 from arm_pytorch_utilities.math_utils import angular_diff_batch
+from pytorch_mppi import mppi
 
 from plant_motion_planning.pybullet_tools.utils import step_simulation, wait_for_duration
 
@@ -17,6 +18,7 @@ angular_params = parsed_yaml['husky_velocity_controller']['angular']['z']
 MAX_VELOCITY = torch.tensor([linear_params['max_velocity'], angular_params['max_velocity']])
 MAX_ACCLERATION = torch.diag(torch.tensor([linear_params['max_acceleration'], angular_params['max_acceleration']]))
 TIME_STEP = 1./240. * 50
+
 STATE_DIM = 5
 CONTROL_DIM = 2
 
@@ -29,7 +31,7 @@ MAX_Y = 5
 MAX_YAW = 2 * np.pi
 
 MIN_LIMITS = torch.tensor([MIN_X, MIN_Y, MIN_YAW, -MAX_VELOCITY[0], -MAX_VELOCITY[1]]).T
-MAX_LIMITS = torch.tensor([MAX_X, MAX_Y, MAX_YAW, MAX_VELOCITY[0], MAX_VELOCITY[0]]).T
+MAX_LIMITS = torch.tensor([MAX_X, MAX_Y, MAX_YAW, MAX_VELOCITY[0], MAX_VELOCITY[1]]).T
 
 def get_dynamics_fn():
     # Dynamics model for husky
@@ -51,10 +53,10 @@ def get_dynamics_fn():
         new_poses = x[:, 0:3] + delta_poses
 
         xnew = torch.cat((new_poses, new_vels), 1)
-        return xnew
+        return xnew[0] if x.dim() == 1 else xnew
     return dynamics_fn
 
-def execute_path(robot, start, path, dynamics_fn, goal, draw_path = False):
+def execute_path(robot, start, path, dynamics_fn, draw_path = False):
     x = start
     if draw_path:
         prev_x = x
@@ -67,103 +69,49 @@ def execute_path(robot, start, path, dynamics_fn, goal, draw_path = False):
             prev_x = x
 
         wait_for_duration(TIME_STEP)
-        cost = get_cost_fn()
 
-def gen_prims(num_quarter_prims, dynamics_fn, draw_prims=False):
-    # End conditions for generating primitives
-    endX = 1.5
-    endTheta = np.pi/2
+def gen_prims(num_prims, dynamics_fn, device, draw_prims=False):
+    prims = []
 
-    u = np.ones(2)
-    prims = list() # list of primitives
+    # Evenly spaced points arround the unit circle
+    starts = torch.zeros((num_prims, STATE_DIM), device=device)
+    goals = torch.zeros((num_prims, STATE_DIM), device=device)
+    goals[:, 2] = torch.arange(0, 2*torch.pi, 2*np.pi / num_prims, device=device)
+    goals[:, 0] = 1.5*torch.cos(goals[:, 2])
+    goals[:, 1] = 1.5*torch.sin(goals[:, 2])
+    
+    # Wrap angles
+    for yaw in goals[:, 2]:
+        if yaw > torch.pi / 2 and yaw < 3 * torch.pi / 2:
+            if yaw < torch.pi:
+                yaw += torch.pi
+            else:
+                yaw -= torch.pi
 
-    # Multiplies for generating prims in all 4 quadrants
-    mult1 = np.array([1, 1]).T
-    mult2 = np.array([1, -1]).T
-    mult3 = np.array([-1, 1]).T
-    mult4 = np.array([-1, -1]).T
-
-    halfway = (int)(num_quarter_prims / 2) # When to switch to decreasing angular control
-
-    # Add primitives from within 4 quadrants
-    # Vary linear velocity first, then vary angular velocity
-    for i in range(0, num_quarter_prims):
-        x = np.zeros(STATE_DIM)
-        prev_x = x
-        prim1 = list()
-        prim2 = list()
-        prim3 = list()
-        prim4 = list()
-
-        counter = 0
-
-        # Keep applying same control input untill primitive is generated
-        while x[0] < endX and x[2] < endTheta:
-            counter += 1
-            x = dynamics_fn(x, u)
-
-            # Make sure to capture all possible controls
-            prim1.append(np.multiply(u, mult1))
-            prim2.append(np.multiply(u, mult2))
-            prim3.append(np.multiply(u, mult3))
-            prim4.append(np.multiply(u, mult4))
-
-            if draw_prims and not (counter % 50):
-                draw_path_line(prev_x, x, (0, 0, 1))
-                prev_x = x
-        
-        # Add primitives to list
-        prims.append(prim1)
-        prims.append(prim2)
-        prims.append(prim3)
-        prims.append(prim4)
-
-        # Reset controls to max at halfway to prep for switch to angular
-        if i == halfway:
-            u = np.ones(2)
-
-        # Decrease corresponding control input (linear is first half angular is second half) 
-        # proportionally to the number of primitives we're generating
-        index = 0 if (i < halfway) else 1
-        u[index] -= (1 / (halfway+1))
-
-    # Add 2 straight motion primitives (one straight forward and one straight back)
-    u = np.zeros(2)
-    u[0] = 1
-    x = np.zeros(STATE_DIM)
-    prev_x = x
-    prim1 = list()
-    prim2 = list()
-
-    while x[0] < endX:
-        x = dynamics_fn(x, u)
-
-        prim1.append(np.multiply(u, mult1))
-        prim2.append(np.multiply(u, mult3))
-
-        if draw_prims:
-            draw_path_line(prev_x, x, (0, 0, 1))
-            prev_x = x
-
-    prims.append(prim1)
-    prims.append(prim2)
-
-    # Add 2 rotating motion primitives (one counterclockwise and one clockwise)
-    u = np.zeros(2)
-    u[1] = 1
-    x = np.zeros(STATE_DIM)
-    prim1 = list()
-    prim2 = list()
-
-    while x[2] < endTheta:
-        x = dynamics_fn(x, u)
-        prim1.append(np.multiply(u, mult1))
-        prim2.append(np.multiply(u, mult2))
-        
-    prims.append(prim1)
-    prims.append(prim2)
+    for start, goal in zip(starts, goals):
+        prim = local_planner(start, goal, dynamics_fn, device, ignore_vel=True)
+        prims.append(prim)
 
     return prims
+
+def local_planner(start, goal, dynamics_fn, device, ignore_vel=False, epsilon=2e-1, max_iterations=1000):
+    cost_fn = get_cost_fn(ignore_vel=ignore_vel)
+    running_cost_fn = get_running_cost_fn(goal, ignore_vel=ignore_vel)
+    ctrl = mppi.MPPI(dynamics=dynamics_fn, running_cost=running_cost_fn, nx=STATE_DIM, noise_sigma=torch.eye(CONTROL_DIM, device=device), \
+                        num_samples=1000, device=device, u_min=-1*torch.ones((1, 2), device=device), u_max=torch.ones((1, 2), device=device))
+
+    x = torch.reshape(start, (1, -1))
+    goal = torch.reshape(goal, (1, -1))
+    controls = torch.empty((0, 2) ,device=device)
+
+    for i in range(max_iterations):
+        u = torch.reshape(ctrl.command(x), (1, -1))
+        controls = torch.cat((controls, u), dim=0)
+        x = dynamics_fn(x, u)
+
+        if cost_fn(x, goal) < epsilon:
+            break
+    return controls
 
 def get_collision_fn():
     # TODO write actual collision 
@@ -176,28 +124,32 @@ def get_sample_fn():
         return np.random.uniform(MIN_LIMITS, MAX_LIMITS)
     return sample_fn
 
-def get_cost_fn():
-    Q = torch.tensor([1, 1, 0.4, 0.1, 0.1])
+def get_cost_fn(ignore_vel=False):
+    Q = torch.tensor([1, 1, 0.3, 3, 0.1])
+    if ignore_vel:
+        Q = Q[0:3]
 
     def cost_fn(x0, x1):
         diff = x0 - x1
+        if ignore_vel:
+            diff = diff[:, 0:3]
         diff[:, 2] = angular_diff_batch(x0[:, 2], x1[:, 2])
-        # diff[:, 2] = torch.minimum(torch.abs(diff[:, 2]), 2*torch.pi - torch.abs(diff[:, 2]))
         cost = diff ** 2 @ Q
         return cost
     return cost_fn
 
 # For use with MPPI controller
-def get_running_cost_fn(dynamics_fn, cost_fn, goal):
+def get_running_cost_fn(goal, ignore_vel=False):
     R = 0.01
+    cost_fn = get_cost_fn(ignore_vel=ignore_vel)
 
     def running_cost_fn(x, u):
-        cost = u.norm(dim=1) * R + cost_fn(x, torch.matmul(torch.ones((x.size(dim=0), 1)), goal))
+        cost = u.norm(dim=1) * R + cost_fn(x, goal.repeat(x.size()[0], 1))
         return cost
     return running_cost_fn
 
 def draw_path_line(x1, x2, color=(0, 0, 1), width = 1.0):
-    draw_line((x1[0], x1[1], 0.01), (x2[0], x2[1], 0.01), width, color)
+    draw_line((x1[0, 0], x1[0, 1], 0.01), (x2[0, 0], x2[0, 1], 0.01), width, color)
 
 def draw_line(start, end, width, color=(0, 0, 1)):
     line_id = p.addUserDebugLine(start, end, color, width)
--- scripts/base_planning.py
+++ scripts/base_planning.py
@@ -6,7 +6,6 @@ from plant_motion_planning.pybullet_tools.kinodynamic_rrt import rrt_solve
 from plant_motion_planning.pybullet_tools.utils import disable_real_time, disconnect, load_model, enable_gravity, set_camera_pose, \
      step_simulation, connect, draw_global_system, HDT_MICHIGAN_URDF, wait_for_user
 from pytorch_mppi import mppi
-import numpy as np
 import torch
 import pybullet as p
 
@@ -18,7 +17,7 @@ disable_real_time()
 # Set camera pose to desired position and orientation
 set_camera_pose((2.5, -1.06, 3.5), (2.5, 2.5, 0.0))
 
-np.random.seed(10)
+#torch.seed(0)
 random.seed(10)
 
 # Draw X, Y, Z axes
@@ -39,23 +38,14 @@ p.addUserDebugText("Goal", goal[0, 0:3], textSize=1, textColorRGB=(0, 1, 0))
 dynamics_fn = get_dynamics_fn()
 sample_fn = get_sample_fn()
 cost_fn = get_cost_fn()
-running_cost_fn = get_running_cost_fn(dynamics_fn, cost_fn, goal)
+running_cost_fn = get_running_cost_fn(goal)
 collision_fn = get_collision_fn()
 
-noise_sigma = torch.tensor([[1., 0.], [0., 1.]], device=d)
-ctrl = mppi.MPPI(dynamics_fn, running_cost_fn, 5, noise_sigma, device=d, u_min=torch.tensor([-1, -1]), u_max=torch.tensor([1, 1]))
-x = start
-path = []
-for i in range(100):
-     u = ctrl.command(x)
-     x = dynamics_fn(x, u)
-     path.append(u)
-execute_path(robot, start, path, dynamics_fn, goal, draw_path=False)
-
-
-# prims = gen_prims(15, dynamics_fn, False)
+prims = gen_prims(13, dynamics_fn, d, False)
 # path = rrt_solve(start, goal, dynamics_fn, collision_fn, cost_fn, sample_fn, prims)
 
+for prim in prims:
+     execute_path(robot, start, prim, dynamics_fn, draw_path=True)
 # if path is not None:
 #     input("Path found! Enter to execute")
 #     execute_path(robot, start, path, dynamics_fn, True)
